234878b95a3df599fb57f56d60ed979416e646fb
Rennan Emanoel <rennanemanoel386@gmail.com>
2021-02-19 13:05:30 -0300

    Versão 1

---
+++ Dr_Remédio/js/main.js
@@ -0,0 +1,30 @@
+function light() {
+    Swal.fire({
+        title: 'Versão Ligth',
+        text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+        imageUrl: "https://blog.vitta.com.br/wp-content/uploads/2020/03/o-que-%C3%A9-preciso-para-se-ter-uma-vida-saud%C3%A1vel-1-1200x675.jpg",
+        confirmButtonText: 'Comprar',
+        
+    });
+}
+function vsuper(){
+    Swal.fire({
+        title: 'Versão Super',
+        text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+        imageUrl: 'https://www.saudevitalidade.com/wp-content/uploads/2020/05/healthy-lifestyle.jpg',
+    });
+}
+function vextreme(){
+    Swal.fire({
+        title: 'Versão Extreme',
+        text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+        imageUrl: 'https://www.dino.com.br/DinoImages/77394a50-984c-4947-a332-dd0291191836.png?quality=100&width=620',
+        confirmButtonText: 'Comprar',
+    });
+}
+function sobre() {
+    Swal.fire({
+        title: 'Sobre',
+        text: 'Nós somos uma instituição que buscar o seu bom estar  Ipsum is simply dummy text of the printing and typesetting industry. Ipsum is simply dummy text of the printing and typesetting industry.',
+    });
+}
